a164a9f5a0ec5adad2a3d8db91cae4fe5215b58b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:11 -0500

    xml commenting

--- src/Soenneker.Communication.Sms.Util/Abstract/IAzureSmsUtil.cs
+++ src/Soenneker.Communication.Sms.Util/Abstract/IAzureSmsUtil.cs
@@ -7,5 +7,12 @@ namespace Soenneker.Communication.Sms.Util.Abstract;
 /// </summary>
 public interface IAzureSmsUtil
 {
+    /// <summary>
+    /// Executes the send operation.
+    /// </summary>
+    /// <param name="from">The from.</param>
+    /// <param name="to">The to.</param>
+    /// <param name="message">The message.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     ValueTask Send(string from, string to, string message);
 }
